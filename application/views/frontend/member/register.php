1e152539fcf22e058c6dc1c24c35ed6451e6f39d
Benny Kindangen <bennykindangen@gmail.com>
2021-06-23 23:40:12 +0800

    updated

--- application/views/frontend/member/register.php
+++ application/views/frontend/member/register.php
@@ -1,7 +1,7 @@
 <section id="content">
     <div class="content-wrap">
         <div class="container">
-            <div class="row">
+            <div class="row register-title-wrap">
                 <div class="col-12 text-center">
                     <h1 class="fastcon-h1 cl-grey-900 text-uppercase">mohon isi formulir pendaftaran</h1>
                 </div>
